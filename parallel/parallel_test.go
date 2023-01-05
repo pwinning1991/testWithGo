c8d648a5dc1d10d2bd52663f85ee9f41ab10225e
Phil <ftcpjw@yahoo.com>
2023-01-04 19:34:15 -0500

    updating

--- parallel/parallel_test.go
+++ parallel/parallel_test.go
@@ -1,18 +1,44 @@
 package parallel
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
-func TestSomething(t *testing.T) {
-	t.Parallel()
+//func TestSomething(t *testing.T) {
+//	t.Parallel()
+//
+//}
 
+//func TestA(t *testing.T) {
+//	t.Parallel()
+//	time.Sleep(time.Second)
+//}
+
+func TestB(t *testing.T) {
+	fmt.Println("setup")
+	defer fmt.Println("deferred teardown")
+	t.Run("group", func(t *testing.T) {
+		t.Run("sub1", func(t *testing.T) {
+			t.Parallel()
+			time.Sleep(time.Second)
+			fmt.Println("sub1 done")
+		})
+		t.Run("sub2", func(t *testing.T) {
+			t.Parallel()
+			time.Sleep(time.Second)
+			fmt.Println("sub2 done")
+		})
+	})
+	fmt.Println("teardown")
 }
 
-func TestA(t *testing.T) {
-	t.Parallel()
-	time.Sleep(time.Second)
+func TestGotcha(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		t.Run(fmt.Sprintf("i=%d", i), func(t *testing.T) {
+			t.Parallel()
+			t.Logf("Testing with i=%d", i)
+		})
+	}
 }
-
-func TestB(t *testing.T) {}
